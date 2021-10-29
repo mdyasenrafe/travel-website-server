e5701b1486dbe3cd8c19b9f837791da53dcd79cd
Yasen30 <mdyasenrafe@gmail.com>
2021-10-29 21:12:33 +0600

    check deploy

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
 const { MongoClient } = require("mongodb");
 
 const port = process.env.PORT || 5000;
+
+// middlecors
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5iwe9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -16,12 +21,17 @@ async function run() {
     await client.connect();
     const database = client.db("touristGang");
     const destinationsCollection = database.collection("destinations");
-
+    const ordersCollection = database.collection("orders");
+    // get api
     app.get("/destinations", async (req, res) => {
       const cursor = destinationsCollection.find();
       const result = await cursor.toArray();
-      console.log(result, "hitting the post");
       res.send(result);
+    });
+    app.post("/orders", async (req, res) => {
+      const orders = req.body;
+      const result = await ordersCollection.insertOne(orders);
+      res.json(result);
     });
 
     console.log("database connect");
